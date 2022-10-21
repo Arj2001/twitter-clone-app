0ff61b2319fa8b5dc77b121467d8eb050975791a
Arj2001 <75932871+Arj2001@users.noreply.github.com>
2022-10-21 12:40:46 +0530

    login/singup ui added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
+const addRoutes:Routes = [
+  {
+    path:"",component:LoginComponent
+  },
+  {
+    path:"signup",component:SignupComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(addRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
